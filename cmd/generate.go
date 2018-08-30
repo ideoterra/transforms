d185adc95a0458c576cfadbbad60cb420d9baf77
Joe <joecolasurdo@eltoro.com>
2018-08-30 09:48:29 -0700

    Some work on the code gen side.

--- cmd/generate.go
+++ cmd/generate.go
@@ -5,8 +5,14 @@ import (
 	"strings"
 )
 
-var primitiveTypes = []string{
-	"int",
+type primitiveType struct {
+	TypeName  string
+	ZeroValue interface{}
+}
+
+var primitiveTypes = []primitiveType{
+	primitiveType{"int", int(0)},
+	primitiveType{"string", ""},
 }
 
 type typeNames struct {
@@ -19,22 +25,68 @@ type typeNames struct {
 func generateTypeNames() []typeNames {
 	result := []typeNames{}
 	for _, primitiveType := range primitiveTypes {
-		result = append(result, typeNames{
-			FileName:      primitiveType + "slice",
-			PrimitiveType: primitiveType,
-			SliceType:     strings.Title(primitiveType) + "Slice",
-			SliceType2:    strings.Title(primitiveType) + "Slice2",
-		})
-		result = append(result, typeNames{
-			FileName:      primitiveType + "slice2",
-			PrimitiveType: strings.Title(primitiveType) + "Slice",
-			SliceType:     strings.Title(primitiveType) + "Slice2",
-			SliceType2:    "[]" + strings.Title(primitiveType) + "Slice2",
-		})
+		oneDimensionalSliceType := typeNames{
+			FileName:      primitiveType.TypeName + "slice.go",
+			PrimitiveType: primitiveType.TypeName,
+			SliceType:     strings.Title(primitiveType.TypeName) + "Slice",
+			SliceType2:    strings.Title(primitiveType.TypeName) + "Slice2",
+		}
+
+		twoDimensionalSliceType := typeNames{
+			FileName:      primitiveType.TypeName + "slice2.go",
+			PrimitiveType: strings.Title(primitiveType.TypeName) + "Slice",
+			SliceType:     strings.Title(primitiveType.TypeName) + "Slice2",
+			SliceType2:    "[]" + strings.Title(primitiveType.TypeName) + "Slice2",
+		}
+
+		result = append(result, oneDimensionalSliceType, twoDimensionalSliceType)
+		log.Println(oneDimensionalSliceType)
+		log.Println(twoDimensionalSliceType)
+	}
+	return result
+}
+
+type conversionNames struct {
+	FileName                string // uses SliceTypeA
+	PrimitiveTypeA          string
+	PrimitiveTypeB          string
+	SliceTypeA              string
+	SliceTypeB              string
+	PrimitiveTypeBZeroValue interface{} // for unit test generation
+}
+
+func generateConversionNames() []conversionNames {
+	result := []conversionNames{}
+	for i, primitiveTypeA := range primitiveTypes {
+		for j, primitiveTypeB := range primitiveTypes {
+			if j == i {
+				continue
+			}
+			oneDimensionalConversion := conversionNames{
+				FileName:                primitiveTypeA.TypeName + "sliceconv.go",
+				PrimitiveTypeA:          primitiveTypeA.TypeName,
+				PrimitiveTypeB:          primitiveTypeB.TypeName,
+				PrimitiveTypeBZeroValue: primitiveTypeB.ZeroValue,
+				SliceTypeA:              strings.Title(primitiveTypeA.TypeName) + "Slice",
+				SliceTypeB:              strings.Title(primitiveTypeB.TypeName) + "Slice",
+			}
+			twoDimensionalConversion := conversionNames{
+				FileName:                primitiveTypeA.TypeName + "slice2conv.go",
+				PrimitiveTypeA:          strings.Title(primitiveTypeA.TypeName) + "Slice",
+				PrimitiveTypeB:          strings.Title(primitiveTypeB.TypeName) + "Slice",
+				PrimitiveTypeBZeroValue: primitiveTypeB.ZeroValue,
+				SliceTypeA:              strings.Title(primitiveTypeA.TypeName) + "Slice2",
+				SliceTypeB:              strings.Title(primitiveTypeB.TypeName) + "Slice2",
+			}
+			result = append(result, oneDimensionalConversion, twoDimensionalConversion)
+			log.Println(oneDimensionalConversion)
+			log.Println(twoDimensionalConversion)
+		}
 	}
 	return result
 }
 
 func main() {
-	log.Println(generateTypeNames())
+	generateTypeNames()
+	generateConversionNames()
 }
--- pkg/slices/generic/conversions.go
+++ pkg/slices/generic/conversions.go
@@ -1,199 +1,23 @@
 package generic
 
-// import (
-// 	"math/big"
+// PrimitiveTypeA is a base type.
+type PrimitiveTypeA interface{}
 
-// 	"github.com/jecolasurdo/transforms/pkg/slices/types"
-// )
+// PrimitiveTypeB is a base type.
+type PrimitiveTypeB interface{}
 
-// //Generic is a placeholder for an interface{}
-// type Generic generic.Type
+// SliceTypeA is a one dimensional slice of PrimitiveTypeA
+type SliceTypeA []PrimitiveTypeA
 
-// //AsUintSlice converts a []Generic to a UintSlice
-// func AsUintSlice(s []Generic, conversion func(value Generic) uint) types.UintSlice {
-// 	r := make(types.UintSlice, len(s))
-// 	for i, v := range s {
-// 		r[i] = conversion(v)
-// 	}
-// 	return r
-// }
+// SliceTypeB is a two dimensional slice of PrimitiveTypeB
+type SliceTypeB []PrimitiveTypeB
 
-// //AsUint8Slice converts a []Generic to a Uint8Slice
-// func AsUint8Slice(s []Generic, conversion func(value Generic) uint8) types.Uint8Slice {
-// 	r := make(types.Uint8Slice, len(s))
-// 	for i, v := range s {
-// 		r[i] = conversion(v)
-// 	}
-// 	return r
-// }
-
-// //AsUint16Slice converts a []Generic to a Uint16Slice
-// func AsUint16Slice(s []Generic, conversion func(value Generic) uint16) types.Uint16Slice {
-// 	r := make(types.Uint16Slice, len(s))
-// 	for i, v := range s {
-// 		r[i] = conversion(v)
-// 	}
-// 	return r
-// }
-
-// //AsUint32Slice converts a []Generic to a Uint32Slice
-// func AsUint32Slice(s []Generic, conversion func(value Generic) uint32) types.Uint32Slice {
-// 	r := make(types.Uint32Slice, len(s))
-// 	for i, v := range s {
-// 		r[i] = conversion(v)
-// 	}
-// 	return r
-// }
-
-// //AsUint64Slice converts a []Generic to a Uint64Slice
-// func AsUint64Slice(s []Generic, conversion func(value Generic) uint64) types.Uint64Slice {
-// 	r := make(types.Uint64Slice, len(s))
-// 	for i, v := range s {
-// 		r[i] = conversion(v)
-// 	}
-// 	return r
-// }
-
-// //AsIntSlice converts a []Generic to an IntSlice
-// func AsIntSlice(s []Generic, conversion func(value Generic) int) types.IntSlice {
-// 	r := make(types.IntSlice, len(s))
-// 	for i, v := range s {
-// 		r[i] = conversion(v)
-// 	}
-// 	return r
-// }
-
-// //AsInt8Slice converts a []Generic to an Int8Slice
-// func AsInt8Slice(s []Generic, conversion func(value Generic) int8) types.Int8Slice {
-// 	r := make(types.Int8Slice, len(s))
-// 	for i, v := range s {
-// 		r[i] = conversion(v)
-// 	}
-// 	return r
-// }
-
-// //AsInt16Slice converts a []Generic to an Int16Slice
-// func AsInt16Slice(s []Generic, conversion func(value Generic) int16) types.Int16Slice {
-// 	r := make(types.Int16Slice, len(s))
-// 	for i, v := range s {
-// 		r[i] = conversion(v)
-// 	}
-// 	return r
-// }
-
-// //AsInt32Slice converts a []Generic to an Int32Slice
-// func AsInt32Slice(s []Generic, conversion func(value Generic) int32) types.Int32Slice {
-// 	r := make(types.Int32Slice, len(s))
-// 	for i, v := range s {
-// 		r[i] = conversion(v)
-// 	}
-// 	return r
-// }
-
-// //AsInt64Slice converts a []Generic to an Int64Slice
-// func AsInt64Slice(s []Generic, conversion func(value Generic) int64) types.Int64Slice {
-// 	r := make(types.Int64Slice, len(s))
-// 	for i, v := range s {
-// 		r[i] = conversion(v)
-// 	}
-// 	return r
-// }
-
-// //AsFloat32Slice converts a []Generic to a Float32Slice
-// func AsFloat32Slice(s []Generic, conversion func(value Generic) float32) types.Float32Slice {
-// 	r := make(types.Float32Slice, len(s))
-// 	for i, v := range s {
-// 		r[i] = conversion(v)
-// 	}
-// 	return r
-// }
-
-// //AsFloat64Slice converts a []Generic to a Float64Slice
-// func AsFloat64Slice(s []Generic, conversion func(value Generic) float64) types.Float64Slice {
-// 	r := make(types.Float64Slice, len(s))
-// 	for i, v := range s {
-// 		r[i] = conversion(v)
-// 	}
-// 	return r
-// }
-
-// //AsComplex64Slice converts a []Generic to a Complex64Slice
-// func AsComplex64Slice(s []Generic, conversion func(value Generic) complex64) types.Complex64Slice {
-// 	r := make(types.Complex64Slice, len(s))
-// 	for i, v := range s {
-// 		r[i] = conversion(v)
-// 	}
-// 	return r
-// }
-
-// //AsComplex128Slice converts a []Generic to a Complex128Slice
-// func AsComplex128Slice(s []Generic, conversion func(value Generic) complex128) types.Complex128Slice {
-// 	r := make(types.Complex128Slice, len(s))
-// 	for i, v := range s {
-// 		r[i] = conversion(v)
-// 	}
-// 	return r
-// }
-
-// //AsByteSlice converts a []Generic to a ByteSlice
-// func AsByteSlice(s []Generic, conversion func(value Generic) byte) types.ByteSlice {
-// 	r := make(types.ByteSlice, len(s))
-// 	for i, v := range s {
-// 		r[i] = conversion(v)
-// 	}
-// 	return r
-// }
-
-// //AsRuneSlice converts a []Generic to a RuneSlice
-// func AsRuneSlice(s []Generic, conversion func(value Generic) rune) types.RuneSlice {
-// 	r := make(types.RuneSlice, len(s))
-// 	for i, v := range s {
-// 		r[i] = conversion(v)
-// 	}
-// 	return r
-// }
-
-// //AsUintptrSlice converts a []Generic to a UintptrSlice
-// func AsUintptrSlice(s []Generic, conversion func(value Generic) uintptr) types.UintptrSlice {
-// 	r := make(types.UintptrSlice, len(s))
-// 	for i, v := range s {
-// 		r[i] = conversion(v)
-// 	}
-// 	return r
-// }
-
-// //AsStringSlice converts a []Generic to a StringSlice
-// func AsStringSlice(s []Generic, conversion func(value Generic) string) types.StringSlice {
-// 	r := make(types.StringSlice, len(s))
-// 	for i, v := range s {
-// 		r[i] = conversion(v)
-// 	}
-// 	return r
-// }
-
-// //AsInterfaceSlice converts a []Generic to an InterfaceSlice
-// func AsInterfaceSlice(s []Generic, conversion func(value Generic) interface{}) types.InterfaceSlice {
-// 	r := make(types.InterfaceSlice, len(s))
-// 	for i, v := range s {
-// 		r[i] = conversion(v)
-// 	}
-// 	return r
-// }
-
-// //AsBigIntSlice converts a []Generic to a BigIntSlice
-// func AsBigIntSlice(s []Generic, conversion func(value Generic) *big.Int) types.BigIntSlice {
-// 	r := make(types.BigIntSlice, len(s))
-// 	for i, v := range s {
-// 		r[i] = conversion(v)
-// 	}
-// 	return r
-// }
-
-// //AsBigFloatSlice converts a []Generic to a BigFloatSlice
-// func AsBigFloatSlice(s []Generic, conversion func(value Generic) *big.Float) types.BigFloatSlice {
-// 	r := make(types.BigFloatSlice, len(s))
-// 	for i, v := range s {
-// 		r[i] = conversion(v)
-// 	}
-// 	return r
-// }
+// AsSliceTypeB applies a converst function to each element in aa and returns a
+// new SliceTypeB.
+func AsSliceTypeB(aa SliceTypeA, convert func(PrimitiveTypeA) PrimitiveTypeB) SliceTypeB {
+	bb := SliceTypeB{}
+	for _, a := range aa {
+		bb = append(bb, convert(a))
+	}
+	return bb
+}
