12f226a480bd91e9be1f2f33324bbebc35a37db7
Joe <joecolasurdo@eltoro.com>
2018-08-30 16:01:23 -0700

    Split constants.

---
+++ pkg/slices/generic/gentypes.go
@@ -0,0 +1,16 @@
+package generic
+
+// This file contains types that are used by the generic package only, and are
+// removed by generate.go for generated packages.
+
+// PrimitiveTypeA is a placeholder used during code generation.
+type PrimitiveTypeA interface{}
+
+// PrimitiveTypeB is a placeholder used during code generation.
+type PrimitiveTypeB interface{}
+
+// SliceTypeA is a placeholder used during code generation.
+type SliceTypeA []PrimitiveTypeA
+
+// SliceTypeB is a placeholder used during code generation.
+type SliceTypeB []PrimitiveTypeB
--- pkg/slices/generic/types.go
+++ pkg/slices/generic/types.go
@@ -6,23 +6,11 @@ type Equality func(a, b PrimitiveType) bool
 // PrimitiveType is the type for SliceType elements.
 type PrimitiveType interface{}
 
-// PrimitiveTypeA is a placeholder used during code generation.
-type PrimitiveTypeA interface{}
-
-// PrimitiveTypeB is a placeholder used during code generation.
-type PrimitiveTypeB interface{}
-
 // SliceType is a one dimensional slice of PrimitiveType.
 type SliceType []PrimitiveType
 
 // SliceType2 is a two dimensional slice of PrimitiveType
 type SliceType2 []SliceType
-
-// SliceTypeA is a placeholder used during code generation.
-type SliceTypeA []PrimitiveTypeA
-
-// SliceTypeB is a placeholder used during code generation.
-type SliceTypeB []PrimitiveTypeB
 
 // Test is a function that conditionally evaluates a PrimitiveType.
 type Test func(PrimitiveType) bool
