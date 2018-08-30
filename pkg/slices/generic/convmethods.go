4ecf7eaa60a7732df4e867b1942a95b6c277d4f0
Joe <joecolasurdo@eltoro.com>
2018-08-30 15:25:57 -0700

    Added conversion method.

---
+++ pkg/slices/generic/convmethods.go
@@ -0,0 +1,11 @@
+package generic
+
+// AsSliceTypeB applies a converst function to each element in aa and returns a
+// new SliceTypeB.
+func (aa *SliceTypeA) AsSliceTypeB(convert func(PrimitiveTypeA) PrimitiveTypeB) *SliceTypeB {
+	return ptrB(AsSliceTypeB(*aa, convert))
+}
+
+func ptrB(aa SliceTypeB) *SliceTypeB {
+	return &aa
+}
