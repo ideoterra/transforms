e62dd3fc873dc517497eb3b7fb8c9b8a16837b60
Joe <joecolasurdo@eltoro.com>
2018-08-30 16:13:14 -0700

    Moved PrimitiveType

--- pkg/slices/generic/gentypes.go
+++ pkg/slices/generic/gentypes.go
@@ -3,14 +3,17 @@ package generic
 // This file contains types that are used by the generic package only, and are
 // removed by generate.go for generated packages.
 
-// PrimitiveTypeA is a placeholder used during code generation.
+// PrimitiveType is the type for SliceType elements.
+type PrimitiveType interface{}
+
+// PrimitiveTypeA is a PrimitiveType used for testing.
 type PrimitiveTypeA interface{}
 
-// PrimitiveTypeB is a placeholder used during code generation.
+// PrimitiveTypeB is a PrimitiveType used for testing.
 type PrimitiveTypeB interface{}
 
-// SliceTypeA is a placeholder used during code generation.
+// SliceTypeA is a one dimensional slice of PrimitiveTypeA used for testing.
 type SliceTypeA []PrimitiveTypeA
 
-// SliceTypeB is a placeholder used during code generation.
+// SliceTypeB is a one dimensional slice of PrimitiveTypeB used for testing.
 type SliceTypeB []PrimitiveTypeB
--- pkg/slices/generic/types.go
+++ pkg/slices/generic/types.go
@@ -3,9 +3,6 @@ package generic
 // Equality is a function that evalutes if two values share something in common.
 type Equality func(a, b PrimitiveType) bool
 
-// PrimitiveType is the type for SliceType elements.
-type PrimitiveType interface{}
-
 // SliceType is a one dimensional slice of PrimitiveType.
 type SliceType []PrimitiveType
 
