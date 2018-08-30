a78e8204db33091464037c99f09b6fa1f2905a81
Joe <joecolasurdo@eltoro.com>
2018-08-30 16:20:57 -0700

    Created p values

--- pkg/slices/generic/const_test.go
+++ pkg/slices/generic/const_test.go
@@ -1,9 +1,11 @@
 package generic_test
 
-// Constants used by tests
+// Constants/vars used by tests
 
 const (
 	primitiveZero   = 0
 	primitiveAValue = 1
 	primitiveBValue = "1"
 )
+
+var p = [10]interface{}{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
