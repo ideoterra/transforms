b2e6b542198b56b1d4208ba05762612df9eb19ad
Joe <joecolasurdo@eltoro.com>
2018-08-30 12:50:23 -0700

    WIP testing methods

---
+++ pkg/slices/generic/methods_test.go
@@ -0,0 +1,18 @@
+package generic_test
+
+import "github.com/jecolasurdo/transforms/pkg/slices/generic"
+
+var testFn = func(a generic.PrimitiveType) (result bool) {
+	return
+}
+
+var equalityFn = func(a, b generic.PrimitiveType) (result bool) {
+	return
+}
+
+var lessFn = func(a, b generic.PrimitiveType) (result boo) {
+	return
+}
+
+var aa = generic.SliceType{}
+var bb = generic.SliceType{}
