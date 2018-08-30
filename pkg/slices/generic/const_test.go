ab7e123eac74389176ada463caa546e197e54637
Joe <joecolasurdo@eltoro.com>
2018-08-30 16:23:53 -0700

    Revert "Created p values"
    This reverts commit a78e8204db33091464037c99f09b6fa1f2905a81.

--- pkg/slices/generic/const_test.go
+++ pkg/slices/generic/const_test.go
@@ -1,11 +1,9 @@
 package generic_test
 
-// Constants/vars used by tests
+// Constants used by tests
 
 const (
 	primitiveZero   = 0
 	primitiveAValue = 1
 	primitiveBValue = "1"
 )
-
-var p = [10]interface{}{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
