d0e230333faff23bee8603e4f9d9453c4e59682a
Joe <joecolasurdo@eltoro.com>
2018-08-30 15:19:55 -0700

    Coverage at 100.

--- pkg/slices/generic/conversions_test.go
+++ pkg/slices/generic/conversions_test.go
@@ -1,1 +1,21 @@
 package generic_test
+
+import (
+	"testing"
+
+	"github.com/jecolasurdo/transforms/pkg/slices/generic"
+	"github.com/stretchr/testify/assert"
+)
+
+var primitiveAZero = 1
+var primitiveBZero = "1"
+
+func TestAsSliceTypeB(t *testing.T) {
+	aa := generic.SliceTypeA{primitiveAZero}
+	bb := generic.SliceTypeB{primitiveBZero}
+	convert := func(a generic.PrimitiveTypeA) generic.PrimitiveTypeB {
+		return primitiveBZero
+	}
+	cc := generic.AsSliceTypeB(aa, convert)
+	assert.ElementsMatch(t, bb, cc)
+}
