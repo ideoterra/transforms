a8f534a250301dfc1f6b76ff17bcde855963139b
Joe Colasurdo <joe@ideoterra.com>
2018-12-03 17:48:10 -0800

    Removed closures package.

--- pkg/slices/generic/closures/closures.go
+++
@@ -1,9 +0,0 @@
-// Package closures contains the definitions of functions that are frequently
-// used as closures in various transformations.
-package closures
-
-// ConditionFn determines whether or not a value meets some condition.
-type ConditionFn func(interface{}) bool
-
-// EqualityFn determins whethre or not two values are equal.
-type EqualityFn func(a, b interface{}) bool
