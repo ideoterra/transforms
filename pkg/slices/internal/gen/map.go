1a8e7307a4a3ff52bca72689aef1e8603f065ab0
Joe Colasurdo <joe@ideoterra.com>
2018-12-04 17:27:52 -0800

    Moved map to own file.

--- pkg/slices/internal/gen/map.go
+++ pkg/slices/internal/gen/map.go
@@ -1,15 +1,5 @@
 package gen
 
-import (
-	"github.com/cheekybits/genny/generic"
-)
-
-// An A is a primitive type.
-type A generic.Type
-
-// B is a primitive type.
-type B generic.Type
-
 // Map contains map functions.
 type Map struct{}
 
---
+++ pkg/slices/internal/gen/shared.go
@@ -0,0 +1,9 @@
+package gen
+
+import "github.com/cheekybits/genny/generic"
+
+// An A is a primitive type.
+type A generic.Type
+
+// B is a primitive type.
+type B generic.Type
