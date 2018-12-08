9977bed40603d4a3d535c967d9a63d6cd2f51b2e
Joe Colasurdo <joe@ideoterra.com>
2018-12-08 08:50:23 -0800

    WIP: enhancing generators for non builtins.

--- pkg/slices/internal/gen/generate.sh
+++ pkg/slices/internal/gen/generate.sh
@@ -10,47 +10,52 @@ done
 
 echo "Generating new transforms..."
 types=(
-	bool
-	rune
-	byte
-	string
-	int
-	int8
-	int16
-	int32
-	int64
-	float32
-	float64
-	uint
-	uint8
-	uint16
-	uint32
-	uint64
-	complex64
-	complex128
-	uintptr
-	error
+	# bool
+	# rune
+	# byte
+	# string
+	# int
+	# int8
+	# int16
+	# int32
+	# int64
+	# float32
+	# float64
+	# uint
+	# uint8
+	# uint16
+	# uint32
+	# uint64
+	# complex64
+	# complex128
+	# uintptr
+	# error
+  # big.Int
+  # big.Float
+  big_Rat
 )
 
 for type_a in ${types[@]}; do
 	for type_b in ${types[@]}; do
     echo "  generating transforms from $type_a to $type_b..."
+    package=x${type_a/_/}
+
 		# Generate code for single-type_a functions.
 		for file in ./*function.1.go; do
 			echo "    $file..."
-			genny -out=../../x$type_a/$file -pkg=x$type_a gen "TA=$type_a" <"$file"
+			genny -out=../../$package/$file -pkg=$package gen "TA=$type_a" <"$file"
 		done
 
 		# Generate code for double-type_a functions.
 		for file in ./*function.2.go; do
 			echo "    $file..."
-			genny -out=../../x$type_a/$file -pkg=x$type_a gen "TA=$type_a TB=$type_b" <"$file"
+			genny -out=../../$package/$file -pkg=$package gen "TA=$type_a TB=$type_b" <"$file"
 		done
 
 		# Generate function containers.
 		for file in ./*container*; do
 			echo "    $file..."
-			genny -out=../../x$type_a/$file -pkg=x$type_a gen "TA=$type_a" <"$file"
+			genny -out=../../$package/$file -pkg=$package gen "TA=$type_a" <"$file"
 		done
 
 	done
